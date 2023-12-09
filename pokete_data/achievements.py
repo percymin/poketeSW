839b805d06d0130f0d5ee1d703a665c7d0ebea3e
percymin <sammymin@naver.com>
2023-12-09 13:25:16 +0900

    seunggi

---
+++ pokete_data/achievements.py
@@ -0,0 +1,43 @@
+"""Contains raw achievement data"""
+
+achievements = {
+    "first_poke": {
+        "title": "First Pokete",
+        "desc": "Catch your first Pokete!"
+    },
+    "first_duel": {
+        "title": "First duel",
+        "desc": "Fight against your first trainer!"
+    },
+    "catch_em_all": {
+        "title": "Catch em all",
+        "desc": "Catch all Poketes and fill your Pokete-Dex!"
+    },
+    "first_evolve": {
+        "title": "First evolution",
+        "desc": "Evolve your first Pokete!"
+    },
+    "first_item": {
+        "title": "First Item use",
+        "desc": "Use your first Item!"
+    },
+    "first_heal_potion": {
+        "title": "First heal potion",
+        "desc": "Use your first heal potion!"
+    },
+    "first_hyperball": {
+        "title": "First hyperball",
+        "desc": "Use your first hyperball!"
+    },
+    
+    "first_superball": {
+        "title": "First superball",
+        "desc": "Use your first superball!"
+    },
+    
+    "first_miss": {
+        "title": "First miss",
+        "desc": "Your first miss of chatching a pokete!"
+    },
+    
+}
